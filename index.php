9b5e20dea4f9bdd5be996531592c83cc44d09374
Andrya <andryavera@gmail.com>
2017-12-16 10:31:30 +0700

    finalize

--- index.php
+++ index.php
@@ -13,76 +13,9 @@ $loader = new Twig_Loader_Filesystem("./templates");
 $twig = new Twig_Environment($loader);
 
 $app->get("/", function () use ($app, $twig) {
-    echo $twig->display("layout.html");
-});
-
-$app->get("/one", function () use ($app, $twig) {
-    echo $twig->display("layout.html");
-});
-
-$app->get("/generate_json", function () use ($app) {
-    $src = array(
-        "header_menu" => array(
-            "hosting",
-            "domain",
-            "server",
-            "website",
-            "affiliasi",
-            "promo",
-            "pembayaran",
-            "review",
-            "kontak",
-            "blog"
-        ),
-        "footer_menu" => array(
-            "layanan" => array(
-                "Domain",
-                "Shared Hosting",
-                "Cloud VPS Hosting",
-                "Managed VPS Hosting",
-                "Web Builder",
-                "Keamanan SSL / HTTPS",
-                "Jasa Pembuatan Website",
-                "Program Affiliasi"
-            ),
-            "Service Hosting" => array(
-                "Hosting Murah",
-                "Hosting Indonesia",
-                "Hosting Singapura SG",
-                "Hosting PHP",
-                "Hosting Wordpress",
-                "Hosting Laravel"
-            ),
-        ),
-        "phone_number" => "0274-5305505",
-        "company_address" => "Jl. Selokan Mataram Monjali Karangjati MT I/304 Sinduadi, Mlati, Sleman Yogyakarta 55284"
-    );
-
-    $res = $app->response;
-
-    $res->headers->set("Content-Type", "application/json");
-    $res->write(json_encode($src));
-
-});
-
-$app->get("/home", function () use ($app, $twig) {
-    echo $twig->display("my_layout.html");
-});
-
-$app->get("/learn1", function () use ($app, $twig) {
-    echo $twig->display("learn1.html");
-});
-
-$app->get("/learn2", function () use ($app, $twig) {
-    echo $twig->display("learn2.html");
-});
-
-$app->get("/learn3", function () use ($app, $twig) {
-    echo $twig->display("learn3.html");
-});
-
-$app->get("/learn4", function () use ($app, $twig) {
-    echo $twig->display("learn4.html");
+    $data_src = file_get_contents('./source/src.json');
+    $data_src = (array) json_decode($data_src);
+    echo $twig->display("layout.html", $data_src);
 });
 
 $app->run();
